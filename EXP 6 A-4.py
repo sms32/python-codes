1d17abad83bd979fcf8c47d8d2b5bb323cf24a97
sms32 <153702953+sms32@users.noreply.github.com>
2024-02-13 16:06:09 +0530

    Update and rename EXP 6 A-1 to EXP 6 A-4.py

--- EXP 6 A-4.py
+++ EXP 6 A-4.py
@@ -1,5 +1,5 @@
 #Write a Python program to get a single string from two given strings,
-separated by a space, and swap the first characters of each string.//
+#separated by a space, and swap the first characters of each string.//
 
 A = input("ENTER A STRING : ")
 print()
